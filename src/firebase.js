b6f2bee0aee0c9c8af564de2919a11d063cdc84b
lu0106 <85585301+lu0106@users.noreply.github.com>
2023-02-09 09:28:56 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIfM7YYXS_jnZmFwqAy80SzrZ1u6uFfw0",
-  authDomain: "project-febf7.firebaseapp.com",
-  projectId: "project-febf7",
-  storageBucket: "project-febf7.appspot.com",
-  messagingSenderId: "359670127486",
-  appId: "1:359670127486:web:8ebdd97acafd7504a0f12f",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
